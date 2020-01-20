3b59250582d69ea7c08289e652621cfdff0569ad
Heberty Carlos <corte_heberty@hotmail.com>
2020-01-19 21:29:33 -0300

    footer

--- resources/views/pages/layouts/blocks/_footer.blade.php
+++ resources/views/pages/layouts/blocks/_footer.blade.php
@@ -1,1 +1,16 @@
-<footer></footer>
+<footer class="footer-site">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 d-flex flex-column flex-md-row justify-content-md-between align-items-center">
+                <a href="/" class="logo"><img src="{{ asset('images/min/logo-black.png') }}" alt="Rits"></a>
+                <div class="box-info">
+                    <strong>Rits Tecnologia. Todos os direitos reservados.</strong>
+                    <p>Desenvolver e evolui soluções digitais para negócios que <br>
+                        acreditam na tecnologia como força propulsora. 
+                    </p>
+                </div>
+                <a href="http://www.rits.com.br" class="link-site" target="_blank">Rits.com.br</a>
+            </div>
+        </div>
+    </div>
+</footer>
--- resources/views/pages/layouts/blocks/_header.blade.php
+++ resources/views/pages/layouts/blocks/_header.blade.php
@@ -2,14 +2,14 @@
     <div class="container">
         <div class="row">
             <div class="col-12 d-flex justify-content-between align-items-center">
-                <a href="/" class="logo"><img src="{{ asset('images/min/logo-white.png') }}" alt=""></a>
-                <a href="#menu" class="menu-link d-lg-none"><img src="{{ asset('images/min/open-menu.svg') }}" alt=""></a>
+                <a href="/" class="logo" title="Rits"><img src="{{ asset('images/min/logo-white.png') }}" alt="Rits"></a>
+                <a href="#menu" class="menu-link d-lg-none" title="Abrir menu"><img src="{{ asset('images/min/open-menu.svg') }}" alt="Abrir menu"></a>
                 <nav id="menu" class="panel nav-site" role="navigation">
-                    <a href="#menu" class="menu-link d-lg-none"><img src="{{ asset('images/min/close.svg') }}" alt=""></a>
+                    <a href="#menu" class="menu-link d-lg-none"><img src="{{ asset('images/min/close.svg') }}" alt="Fechar menu"></a>
                     <ul>
-                        <li><a href="javascript:;">A Rits</a></li>
-                        <li><a href="javascript:;">Nosso Valores</a></li>
-                        <li><a href="javascript:;" class="btn-site btn-header">Vagas Abertas</a></li>
+                        <li><a href="javascript:;" title="A Rits">A Rits</a></li>
+                        <li><a href="javascript:;" title="Nosso Valores">Nosso Valores</a></li>
+                        <li><a href="javascript:;" class="btn-site btn-header" title="Vagas Abertas">Vagas Abertas</a></li>
                     </ul>
                 </nav>
             </div>
